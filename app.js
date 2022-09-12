8c078432031b773f91f3ebe5d7f10622ae82e313
Mohammed Ramadan <mohammed.ramadan1474@gmail.com>
2022-09-12 22:34:24 +0200

    feat(gameboard): :sparkles: add gameboard module
    handles everything related to the board logic

--- app.js
+++ app.js
@@ -9,3 +9,24 @@ const playerFactory = (sign) => {
 
   return { getSign };
 };
+
+const gameBoard = (function () {
+  const _boardArray = ["", "", "", "", "", "", "", "", ""];
+
+  const addToBoardArray = (sign, index) => {
+    _boardArray.splice(index, 1, sign);
+  };
+
+  const getBoardArray = () => {
+    return _boardArray;
+  };
+
+  const reset = () => {
+    for (let i = 0; i < _boardArray.length; i++) {
+      _boardArray[i] = "";
+    }
+  };
+
+  return { addToBoardArray, getBoardArray, reset };
+})();
+
